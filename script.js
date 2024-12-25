283b5ffd6f47b52191bf96858c267069c97f8a87
ColdAndromeda <bensonzheng87901@gmail.com>
2024-12-24 22:24:29 -0500

    Js defer

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const digit = document.querySelectorAll(".digit");
-const operator = document.querySelectorAll(".operator");
-const buttons = document.querySelectorAll(".buttons");
-
+const display = document.querySelector(".display");
 let numberStack = [];
 
 const addNumOperation = () => {
   document.addEventListener("click", (e) => {
     if (e.target.matches("button")) {
-      console.log(e.target);
+      display.textContent = e.target.textContent;
     }
   });
 };
