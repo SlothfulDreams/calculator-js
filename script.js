af6553c0f79811a921711ddc9aa6d7dcc3a86c93
ColdAndromeda <bensonzheng87901@gmail.com>
2024-12-25 22:53:26 -0500

    Scaffolding Functions

--- script.js
+++ script.js
@@ -4,9 +4,31 @@ let numberStack = [];
 const addNumOperation = () => {
   document.addEventListener("click", (e) => {
     if (e.target.matches("button")) {
-      display.textContent = e.target.textContent;
+      if (numberStack.length < 0) {
+        display.textContent = e.target.textContent;
+        numberStack.push(e.target.textContent);
+      }
     }
   });
 };
 
+const add = () => {
+  // adds the numbers
+};
+
+const substract = () => {
+  // substracts
+};
+
+const division = () => {
+  // Division
+};
+
+const multiply = () => {
+  // multiply
+};
+
+const operate = () => {
+  // Takes two numbers then calls one of the above functions
+};
 addNumOperation();
