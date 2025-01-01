fd17e21d6003e7badd6cab45c51c5482b1323a0c
ColdAndromeda <bensonzheng87901@gmail.com>
2024-12-31 23:22:11 -0500

    clear functionality

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 const display = document.querySelector(".display");
-let numberStack = [];
+let num = "";
 
 const addNumOperation = () => {
   document.addEventListener("click", (e) => {
-    if (e.target.matches("button")) {
-      if (display.textContent == "0") {
-        display.textContent = e.target.textContent;
-        numberStack.push(e.target.textContent);
-        console.log(numberStack);
+    if (e.target.className == "digit") {
+      if (num.length < 13) {
+        num += e.target.textContent;
+        display.textContent = num;
       }
+    }
+    if (e.target.className == "operation") {
+      num = "";
+    }
+
+    if (e.target.className == "clear") {
+      num = "";
+      display.textContent = 0;
     }
   });
 };
 
 const add = (num1, nums2) => {
-  return Number(num1) + Number(num2);
+  return num1 + num2;
 };
 
 const subtract = (num1, nums2) => {
-  return Number(num1) - Number(num2);
+  return num1 - num2;
 };
 
 const multiply = (num1, nums2) => {
-  return Number(num1) * Number(num2);
+  return num1 * num2;
 };
 
 const divide = (num1, nums2) => {
-  return Number(num1) / Number(num2);
+  return num1 / num2;
 };
 
 const operate = (num1, num2, operation) => {
-  // Takes two numbers then calls one of the above functions
-};
-
-const clear = () => {
-  numberStack.length = 0;
+  // Takes two s then calls one of the above functions
 };
 
 addNumOperation();
+
+// Wait for user to hit a different operation that is not a number
+// Store the number after that operation then
+// when user hits = total tyhe operation up
