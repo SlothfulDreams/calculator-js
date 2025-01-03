27ef89ea02102e27b89410f07e833c8d5fe7e689
ColdAndromeda <bensonzheng87901@gmail.com>
2025-01-02 23:12:29 -0500

    Improve logic

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 const display = document.querySelector(".display");
 let num = "";
+const numberStack = [];
+const operations = ["/", "-", "+", "*"];
 
 const addNumOperation = () => {
   document.addEventListener("click", (e) => {
     if (e.target.className == "digit") {
-      if (num.length < 13) {
-        num += e.target.textContent;
-        display.textContent = num;
+      if (numberStack.length < 13) {
+        if (num.length == 0) {
+          num += e.target.textContent;
+          display.textContent = num;
+          numberStack.push(e.target.textContent);
+          console.log(numberStack);
+        } else {
+          const lastDigit = numberStack.pop();
+          const inputNumber = e.target.textContent;
+          display.textContent = lastDigit + inputNumber;
+          numberStack.push(lastDigit + inputNumber);
+          console.log(numberStack);
+        }
       }
     }
-    if (e.target.className == "operation") {
+
+    if (e.target.className == "operator") {
       num = "";
+      if (operations.includes(numberStack.at(-1))) {
+        display.textContent = NaN;
+        numberStack.length = 0;
+        console.log(numberStack, "bnasdsadas");
+      } else {
+        numberStack.push(e.target.textContent);
+        console.log(numberStack, "pushing");
+      }
     }
 
     if (e.target.className == "clear") {
       num = "";
+      numberStack.length = 0;
       display.textContent = 0;
     }
 
@@ -45,7 +67,3 @@ const operate = (num1, num2, operation) => {
 };
 
 addNumOperation();
-
-// Wait for user to hit a different operation that is not a number
-// Store the number after that operation then
-// when user hits = total tyhe operation up
